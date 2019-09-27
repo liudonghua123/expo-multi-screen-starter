6f87b80e7bee00cf23708ea87d6ddc187c7e1fe3
liudonghua123 <liudonghua123@gmail.com>
2019-09-27 16:10:56 +0800

    add ImagePicker sample code

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { ScrollView, Text, View } from 'react-native';
+import { ScrollView, Text, View, Image, Button } from 'react-native';
 import { useTheme } from 'react-navigation';
+import * as ImagePicker from 'expo-image-picker';
+import Constants from 'expo-constants';
+import * as Permissions from 'expo-permissions';
 import { gStyle } from '../constants';
 
 // components
@@ -10,12 +13,53 @@ import Touch from '../components/Touch';
 const HomeScreen = ({ navigation, screenProps }) => {
   const theme = useTheme();
 
+  // a sample code on how to use imagepicker to select image
+  // https://docs.expo.io/versions/latest/sdk/imagepicker/
+  // https://daveceddia.com/intro-to-hooks/
+  const [image, setImage] = useState(null);
+
+  const getPermissionAsync = async () => {
+    if (Constants.platform.ios) {
+      const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+      if (status !== 'granted') {
+        alert('Sorry, we need camera roll permissions to make this work!');
+      }
+    }
+  };
+  const _pickImage = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3]
+    });
+    console.log(result);
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+  // https://stackoverflow.com/questions/53945763/componentdidmount-equivalent-on-a-react-function-hooks-component
+  useEffect(() => {
+    getPermissionAsync();
+  }, []);
+
   return (
     <View style={gStyle.container[theme]}>
       <ScrollView contentContainerStyle={gStyle.contentContainer}>
         <Text style={gStyle.text[theme]}>Home content area</Text>
 
         <View style={gStyle.spacer64} />
+
+        <View
+          style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+        >
+          <Button title="Pick an image from camera roll" onPress={_pickImage} />
+          {image && (
+            <Image
+              source={{ uri: image }}
+              style={{ width: 200, height: 200 }}
+            />
+          )}
+        </View>
 
         <Touch
           onPress={() =>
